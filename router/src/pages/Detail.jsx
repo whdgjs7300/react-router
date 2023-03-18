33254ac826e853fca8fd21e3fa2230d27db19a7f
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-18 16:08:54 +0900

    3/18 useParams

--- router/src/pages/Detail.jsx
+++ router/src/pages/Detail.jsx
@@ -1,9 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const Detail = () => {
+    // 오브젝트 값으로 받아옴 (id , num  :뒤에있는 키값을 오브젝트 값)
+    const {id} = useParams();
+    console.log(id)
     return ( 
         <div>
-            <h1>디테일 페이지</h1>
+            <h1>디테일 페이지 {id}</h1>
         </div>
-     );
+    );
 }
- 
+
 export default Detail;
