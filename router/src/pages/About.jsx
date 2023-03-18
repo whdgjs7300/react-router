65c403a7102fc9270c7378c86ec25f7ac4d0d6e5
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-18 15:26:26 +0900

    3/18 useNavigate

--- router/src/pages/About.jsx
+++ router/src/pages/About.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+    const navigate = useNavigate();
 
+    const goToHomepage = () => {
+        navigate('/')
+    }
+    
     return (  
         <div>
             <h1>about페이지</h1>
+            <button onClick={goToHomepage}>go to Homepage</button>
         </div>
     );
 }
--- router/src/pages/Homepage.jsx
+++ router/src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 
 
 const Homepage = () => {
     return ( 
         <div>
             <h1>홈페이지입니다</h1>
+            <Link to='/about'>about</Link>
         </div>
     );
 }
