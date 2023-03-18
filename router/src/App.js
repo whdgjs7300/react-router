c9131ca5833aca6efb5dbe03ae3758c7da7b1b61
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-18 15:15:54 +0900

    3/18 Routes

--- router/src/App.js
+++ router/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from './pages/Homepage';
 import About from './pages/About';
+import { Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
-      <About/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}></Route>
+        <Route path='/about' element={<About/>} ></Route>
+      </Routes>
+      
     </div>
   );
 }
