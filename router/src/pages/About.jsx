2f4fbff7a784a2a451210193ae8d516dcf53c9e4
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-18 15:08:46 +0900

    3/18 routerSet

---
+++ router/src/pages/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+
+    return (  
+        <div>
+            <h1>about페이지</h1>
+        </div>
+    );
+}
+
+export default About;
---
+++ router/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+
+
+const Homepage = () => {
+    return ( 
+        <div>
+            <h1>홈페이지입니다</h1>
+        </div>
+    );
+}
+
+export default Homepage;
